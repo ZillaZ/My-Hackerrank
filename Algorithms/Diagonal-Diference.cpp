13804389d187cdc3044d4188689c371ee59fe0a0
Lucas Henrique <lucasmula125@gmail.com>
2022-02-19 00:46:00 -0300

    My solution

---
+++ Algorithms/Diagonal-Diference.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/* My code below */
+
+int diagonalDifference(vector<vector<int>> arr) {
+    int l = 0; int r = 0;
+    
+    for(int i = 0,j = 0; i < arr.size(); i++, j++){
+        l += arr.at(i).at(j);
+    }
+
+    for(int i = arr.size()-1, j = 0; i >= 0; i--, j++){
+        r += arr.at(i).at(j);
+    }
+    
+    return abs(l - r);
+}
+
+/* ends here */
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<vector<int>> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        arr[i].resize(n);
+
+        string arr_row_temp_temp;
+        getline(cin, arr_row_temp_temp);
+
+        vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
+
+        for (int j = 0; j < n; j++) {
+            int arr_row_item = stoi(arr_row_temp[j]);
+
+            arr[i][j] = arr_row_item;
+        }
+    }
+
+    int result = diagonalDifference(arr);
+
+    cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
